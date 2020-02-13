8980e04c6d0378fd0a113ad09cfbec2df3836a0b
IanJPeek <IanJPeek@gmail.com>
2020-02-13 11:49:45 +0000

    solve and test alternating case, including long sentences with mixed punctuation

--- katas/alternate-case.js
+++ katas/alternate-case.js
@@ -4,8 +4,137 @@
   
   'hello' would become 'HeLlO'
   'hello world' would become 'HeLlO wOrLd'
-*/
+  */
 
-function alternateCase() {}
+function alternateCase(str) {
+  if (str.length === 0) {
+    return "";
+  }
+
+  const letters = str.split("");
+  const caseArray = [];
+  let spaceAdjust = false;
+
+  for (i = 0; i < letters.length; i++) {
+    if (
+      letters[i] === " " ||
+      letters[i] === "," ||
+      letters[i] === ";" ||
+      letters[i] === "'"
+    ) {
+      spaceAdjust = !spaceAdjust;
+    }
+    
+    if (spaceAdjust === false) {
+      i % 2 === 0
+      ? caseArray.push(letters[i].toUpperCase())
+      : caseArray.push(letters[i].toLowerCase());
+    }
+    
+    if (spaceAdjust === true) {
+      i % 2 === 0
+      ? caseArray.push(letters[i].toLowerCase())
+      : caseArray.push(letters[i].toUpperCase());
+    }
+  }
+  const joined = caseArray.join("");
+  return joined;
+  
+
+  //Single Words - works - starter solution
+    // for (i = 0; i < letters.length; i++) {
+    //   i % 2 === 0
+    //     ? caseArray.push(letters[i].toUpperCase())
+    //     : caseArray.push(letters[i].toLowerCase());
+    // }
+  
+    // const joined = caseArray.join("");
+    // return joined;
+  
+    // Adjust Try method
+  
+
+  //Several Words - initial attempts
+  // const wordArr = str.split(" ");
+  // console.log(wordArr);
+  
+  // function caser(word) {
+  //   let casedWord = [];
+  //   for (i = 0; i < word.length; i++) {
+  //     i % 2 === 0
+  //       ? casedWord.push(word[i].toUpperCase())
+  //       : casedWord.push(word[i].toLowerCase());
+  //   }
+  //   console.log(casedWord);
+  //   caseArray.push(casedWord.join(""));
+  // }
+
+  // function altCaser(word) {
+  //   let casedWord = [];
+  //   for (i = 0; i < word.length; i++) {
+  //     i % 2 === 1
+  //       ? casedWord.push(word[i].toUpperCase())
+  //       : casedWord.push(word[i].toLowerCase());
+  //   }
+  //   console.log(casedWord);
+  //   caseArray.push(casedWord.join(""));
+  // }
+
+  // function dualCaser() {}
+
+  // console.log(wordArr.forEach(caser), caseArray, "return");
+
+  // // console.log(casedWord, caseArray);
+
+  // for (i = 0; i < caseArray.length; i++) {
+  //   if (i > 0 && caseArray[i - 1].length % 2 === 1) {
+  //   }
+  // }
+
+  return caseArray;
+
+  //EG FOR EACH
+
+  // var fruits = ["apple", "orange", "cherry"];
+  // fruits.forEach(myFunction);
+
+  // function myFunction(item, index) {
+  //   document.getElementById("demo").innerHTML += index + ":" + item + "<br>";
+  // }
+
+  //  const upped = "" + str.toUpperCase()
+  //  console.log(upped)
+
+  // return upped
+
+  //old
+
+  //  const alternatingWord = [];
+  //  for (let i = 0; i < str.length; i++) {
+  //    if (i % 2 === 0) {
+  //      alternatingWord.push(str[i].toUpperCase());
+  //    } else if (i % 2 === 1) {
+  //      alternatingWord.push(str[i].toLowerCase());
+  //    }
+  //  }
+  //  return alternatingWord.join("");
+
+  //New
+
+  //     console.log(letters, noWhite)
+
+  // caseArray.push(noWhite[0].toUpperCase())
+
+  // console.log(caseArray[0])
+  // const ans = "" + caseArray[0]
+
+  // return ans
+
+  // const cased = noWhite.map(
+  //   letter => console.log(noWhite.indexOf(letter)))
+
+  // console.log (letters, noWhite, caseArray)
+  // return caseArray
+}
 
 module.exports = alternateCase;
--- spec/alternate-case.spec.js
+++ spec/alternate-case.spec.js
@@ -1,7 +1,46 @@
-const alternateCase = require('../katas/alternate-case');
+const { expect } = require("chai");
+const alternateCase = require("../katas/alternate-case");
 
-describe('alternateCase', () => {
-  it('describe what happens if this test succeeds here', () => {
-    // expect(alternateCase(???)).to...
+describe("alternateCase", str => {
+  it("returns an empty string unchanged", () => {
+    expect(alternateCase("")).to.equal("");
+  });
+
+  it("returns a single letter as upperCase", () => {
+    expect(alternateCase("a")).to.equal("A");
+  });
+
+  it("returns a two-letter string in alternate case", () => {
+    expect(alternateCase("eH")).to.equal("Eh");
+  });
+
+  it('returns longer words in alternating case', () => {
+     expect(alternateCase("testing")).to.equal("TeStInG");
+      expect(alternateCase("chImPANzEE")).to.equal("ChImPaNzEe");
+  });
+
+  it('returns several words in alternate cases where different starting cases are required', () => {
+    expect(alternateCase("chocolate marshmallow pie")).to.equal("ChOcOlAtE mArShMaLlOw PiE")
+  });
+
+  it("returns many words in alternate cases with varying length words and longer sentences", () => {
+    expect(
+      alternateCase("By golly we might just have a working solution")
+    ).to.equal("By GoLlY wE mIgHt JuSt HaVe A wOrKiNg SoLuTiOn");
+  })
+
+  it("returns many words in alternate cases with varying length words and longer (punctuated) sentences", () => {
+    expect(
+      alternateCase(
+        "Gosh and darn, I can barely believe it; it's only gone and worked!"
+      )
+    ).to.equal(
+      "GoSh AnD dArN, i CaN bArElY bElIeVe It; It'S oNlY gOnE aNd WoRkEd!"
+    );
   });
 });
+
+//  let multipleWords = "chocolate marshmallow pie";
+//  let actualResult = alternateCase(multipleWords);
+//  let expectedResult = "ChOcOlAtE mArShMaLlOw PiE";
+//  expect(actualResult).to.equal(expectedResult);
